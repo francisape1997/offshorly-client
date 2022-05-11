bf1640f759feb7b962db9a7b55ea6bc5b20a40b4
Francis <90559251+francisape1997@users.noreply.github.com>
2022-05-11 18:57:00 +0800

    add main layout

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <TopNav class="mb-5"/>
+
+        <router-view/>
+    </div>
+</template>
+
+<script>
+
+// Top Nav
+import TopNav from '../components/header/index.vue';
+
+export default {
+    name: 'MainLayout',
+
+    components: {
+        TopNav,
+    },
+}
+</script>
