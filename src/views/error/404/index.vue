76d8bc6f1554a011a6ab676881ee08b8de047e36
Francis <90559251+francisape1997@users.noreply.github.com>
2022-05-11 18:51:38 +0800

    Add 404 error page

---
+++ src/views/error/404/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <img src="../../../assets/404.svg" alt="404 Not Found" class="center">
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Not Found',
+};
+</script>
+
+<style scoped>
+.center {
+  width: 80%;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+</style>
