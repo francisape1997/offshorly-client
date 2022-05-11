160556c4c205d77876d675471dfbe7924697bba4
Francis <90559251+francisape1997@users.noreply.github.com>
2022-05-11 18:57:14 +0800

    add router

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,83 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+
+// Layout
+import MainLayout from '../layouts/MainLayout.vue';
+
+// Pages
+import Notes from '../views/notes/index.vue';
+import NoteForm from '../views/notes/form.vue';
+
+const routes = [
+    { path: '/:pathMatch(.*)*', name: 'Not Found', component: () => import('../views/error/404/index.vue') },
+
+    // Login
+    { path: '/login', component: () => import('../views/auth/index.vue'), meta: { page: 'login' } },
+
+    // Register
+    {
+        path: '/',
+        component: MainLayout,
+        children:
+        [
+            {
+                path: 'register',
+                component: () => import('../views/auth/register.vue'), meta: { page: 'register' },
+            }
+        ],
+    },
+
+    // Notes
+    {
+        path: '/',
+        component: MainLayout,
+        children:
+        [
+            {
+                path: 'notes',
+                component: Notes,
+            },
+            {
+                path: 'note/create',
+                component: NoteForm,
+                meta:
+                {
+                    page: 'create',
+                },
+            },
+            {
+                path: 'note/:id/edit',
+                component: NoteForm,
+            }
+        ],
+    },
+];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+  history: createWebHistory(),
+  routes,
+});
 
-export default router
+router.beforeEach((to, from, next) =>
+{
+  const notAuthRequired = ['login', 'register'];
+
+  const hasAccessToken = localStorage.getItem('token') !== null;
+
+  const authRequired = !notAuthRequired.includes(to.meta.page);
+
+  if (authRequired && !hasAccessToken)
+  {
+      return next('/login');
+  }
+
+  if (hasAccessToken && !authRequired)
+  {
+      return next('/notes');
+  }
+
+  if (to.path === '/') return next('/notes');
+
+  return next();
+});
+
+export default router;
