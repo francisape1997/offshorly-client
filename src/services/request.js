8872f846b4174cfc6f56270938362cff11221b84
Francis <90559251+francisape1997@users.noreply.github.com>
2022-05-13 02:33:20 +0800

    add production env

--- src/services/request.js
+++ src/services/request.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export function request(url, method, data = [])
 {
-    const API_URL = 'http://offshorly.local/api' // Your api hostname here
+    const API_URL = import.meta.env.VITE_APP_API_BASE_URL;
 
     return axios({
         url: `${API_URL}/${url}`,
